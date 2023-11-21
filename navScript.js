9fe6cf8c773db9424db400543196e2f28d297c8b
coming <109262220+kiwicrack@users.noreply.github.com>
2023-11-21 11:21:50 +0800

    Nav Bar Frame

---
+++ navScript.js
@@ -0,0 +1,8 @@
+function showNav(){
+    document.querySelector('.hamburger').classList.toggle('open')
+    document.querySelector('.navigation').classList.toggle('active')
+}
+
+function showOver(){
+    document.querySelector('.overlay').classList.toggle('active')
+}
